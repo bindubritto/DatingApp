c850cb0fa362aa24d748247747789d8b6a125fa0
sabbir-bs <sabbir.ah@brainstation23.com>
2020-08-22 23:35:50 +0600

    Pagination, searching & sorting added.

--- DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -9,11 +9,13 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 
 export class MemberListResolver implements Resolve<User[]>{
+    pageNumber = 1;
+    pageSize = 5;
     constructor(private userService: UserService, private router: Router, private alertify: AlertifyService){}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
         // calling user service to get the user matching with this id. and rest of this is catching error.
-        return this.userService.getUsers().pipe(
+        return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
             catchError(error => {
                 this.alertify.error('Problem retrieving data');
                 this.router.navigate(['/home']);
